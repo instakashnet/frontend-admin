078daba9cfddffad864f90c9300917ecc34323b5
Roger Rengifo <thelea12@gmail.com>
2021-03-02 12:07:12 -0500

    socket connection

--- src/socketconnection.js
+++ src/socketconnection.js
@@ -1,8 +1,4 @@
 import io from "socket.io-client";
-const socket = io(process.env.NODE_ENV === "production" ? "https://instakash-exchange-service.herokuapp.com/orders" : "https://exchange-service-on36n.ondigitalocean.app/orders", {
-  reconnection: true,
-  reconnectionAttempts: 3,
-  reconnectionDelayMax: 10000,
-});
+const socket = io(process.env.NODE_ENV === "production" ? "https://instakash-exchange-service.herokuapp.com/orders" : "https://exchange-service-on36n.ondigitalocean.app/orders");
 
 export default socket;
