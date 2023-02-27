060d7f66283fd9a3b1706012835adc423948319f
Roger Rengifo <rogerrengifo@Rogers-MacBook-Pro.local>
2023-02-27 18:22:21 -0500

    fixing order details and adding user info button

--- src/pages/orders/components/details/user-details.component.js
+++ src/pages/orders/components/details/user-details.component.js
@@ -1,60 +1,60 @@
-import { Person } from "@material-ui/icons";
-import React from "react";
-import { Link } from "react-router-dom";
-import { Card, CardBody, Media } from "reactstrap";
+import { Person } from '@material-ui/icons';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Card, CardBody, Media } from 'reactstrap';
 
-import Company from "../../../../assets/images/profile-company.svg";
-import Female from "../../../../assets/images/profile-female.svg";
-import Male from "../../../../assets/images/profile-male.svg";
-import CopyButton from "../../../../components/UI/CopyButton";
-import { SkeletonComponent } from "../../../../components/UI/skeleton.component";
+import Company from '../../../../assets/images/profile-company.svg';
+import Female from '../../../../assets/images/profile-female.svg';
+import Male from '../../../../assets/images/profile-male.svg';
+import CopyButton from '../../../../components/UI/CopyButton';
+import { SkeletonComponent } from '../../../../components/UI/skeleton.component';
 
 const User = ({ user, role, isLoading }) => {
   let Avatar;
 
   if (user) {
-    Avatar = user.type === "juridica" ? Company : Male;
-    if (user.type === "natural") Avatar = user.identitySex === "male" ? Male : Female;
+    Avatar = user.type === 'juridica' ? Company : Male;
+    if (user.type === 'natural') Avatar = user.identitySex === 'male' ? Male : Female;
   }
 
   return (
     <Card>
       <CardBody>
-        <Media className="transacion-details">
+        <Media className='transacion-details'>
           {isLoading ? (
             <UserLoading />
           ) : (
             <>
-              <div className="mr-3">
-                <img src={Avatar} width={35} className="avatar-md rounded-circle img-thumbnail" alt="user" />
+              <div className='mr-3'>
+                <img src={Avatar} width={35} className='avatar-md rounded-circle img-thumbnail' alt='user' />
               </div>
-              <Media body className="align-self-center">
-                <div className="text-muted">
+              <Media body className='align-self-center'>
+                <div className='text-muted'>
                   <h5>
-                    {user.firstName + " " + user.lastName}
-                    {(role === "admin" || role === "manager") && (
-                      <Link to={`/user-details/${user.userId}`} className="ml-2 relative bottom-0.5">
+                    {user.firstName + ' ' + user.lastName}
+                    {(role === 'admin' || role === 'manager' || role === 'orders') && (
+                      <Link to={`/user-details/${user.userId}`} className='ml-2 relative bottom-0.5'>
                         <Person />
                       </Link>
                     )}
                   </h5>
-                  <p className="mb-1">
+                  <p className='mb-1'>
                     {user.email} <CopyButton textToCopy={user.email} />
                   </p>
-                  <p className="mb-1">
+                  <p className='mb-1'>
                     <b>Documento:</b> {`${user.documentType} ${user.documentIdentification}`}
                   </p>
-                  <p className="mb-0">
+                  <p className='mb-0'>
                     <b>Teléfono:</b> {user.phone} <CopyButton textToCopy={user.phone} />
                   </p>
                 </div>
               </Media>
-              {user.type === "juridica" && (
-                <Media body className="align-self-center">
-                  <div className="text-muted">
+              {user.type === 'juridica' && (
+                <Media body className='align-self-center'>
+                  <div className='text-muted'>
                     <h5>Empresa</h5>
-                    <p className="mb-1">{user.razonSocial}</p>
-                    <p className="mb-0">
+                    <p className='mb-1'>{user.razonSocial}</p>
+                    <p className='mb-0'>
                       <b>RUC:</b> {user.ruc}
                     </p>
                   </div>
@@ -70,14 +70,14 @@ const User = ({ user, role, isLoading }) => {
 
 const UserLoading = () => {
   return (
-    <div className="flex items-center">
-      <SkeletonComponent variant="circle" animation="wave" width={80} height={80} />
-      <div className="ml-3">
-        <SkeletonComponent variant="text" width={220} />
+    <div className='flex items-center'>
+      <SkeletonComponent variant='circle' animation='wave' width={80} height={80} />
+      <div className='ml-3'>
+        <SkeletonComponent variant='text' width={220} />
         <br />
-        <SkeletonComponent variant="text" />
-        <SkeletonComponent variant="text" />
-        <SkeletonComponent variant="text" />
+        <SkeletonComponent variant='text' />
+        <SkeletonComponent variant='text' />
+        <SkeletonComponent variant='text' />
       </div>
     </div>
   );
