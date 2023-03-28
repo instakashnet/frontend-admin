5cd300ae4b5382ec731ebcf5cb84d3c1d872dbf8
Roger Rengifo <rogerrengifo@ip-192-168-18-6.ec2.internal>
2023-03-27 23:32:21 -0500

    vite mode prod dev

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,32 +1,34 @@
-import axios from 'axios';
-import { setAxiosInterceptor } from './interceptors';
+import axios from 'axios'
+import { setAxiosInterceptor } from './interceptors'
 
-const BASE_URL = process.env.REACT_APP_STAGE === 'prod' ? 'https://api.instakash.net' : 'https://api.dev.instakash.net';
+const BASE_URL = import.meta.env.PROD
+  ? 'https://api.instakash.net'
+  : 'https://api.dev.instakash.net'
 
 const instancesURL = {
   exchange: {
     v1: `${BASE_URL}/exchange-service/api/v1/admin`,
-    v2: `${BASE_URL}/exchange-service/api/v2/admin`,
+    v2: `${BASE_URL}/exchange-service/api/v2/admin`
   },
   auth: {
-    v1: `${BASE_URL}/auth-service/api/v1/admin`,
+    v1: `${BASE_URL}/auth-service/api/v1/admin`
   },
   accounts: {
-    v1: `${BASE_URL}/accounts-service/api/v1/admin`,
+    v1: `${BASE_URL}/accounts-service/api/v1/admin`
   },
   base: {
-    v1: BASE_URL,
-  },
-};
+    v1: BASE_URL
+  }
+}
 
 export const getAxiosInstance = (instanceName, version) => {
   const instance = axios.create({
     baseURL: instancesURL[instanceName][version],
     withCredentials: true,
-    timeout: 30000,
-  });
+    timeout: 30000
+  })
 
-  setAxiosInterceptor(instance);
+  setAxiosInterceptor(instance)
 
-  return instance;
-};
+  return instance
+}
